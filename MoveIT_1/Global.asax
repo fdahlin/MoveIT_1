e5246b8e32af03856e3d1365173c1ebcfbf93fc8
fdahlin <fredrick.dahlin@gmail.com>
2015-03-21 12:45:05 +0100

    init

---
+++ MoveIT_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoveIT_1.MvcApplication" Language="C#" %>
---
+++ MoveIT_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoveIT_1.Startup))]
+namespace MoveIT_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
